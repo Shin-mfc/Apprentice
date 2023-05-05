1514631a51e859947087db7f9ea089b48d4f4810
Shin <84771278+Shin-mfc@users.noreply.github.com>
2023-05-06 00:40:31 +0900

    ステップ３

--- Week3-4/FinalQuest/password_manager.zsh
+++ Week3-4/FinalQuest/password_manager.zsh
@@ -1,5 +1,18 @@
 # ファイル名の設定
 password_file="passwords.txt"
+encrypted_password_file="passwords.txt.gpg"
+
+# GPG暗号化用パスワード
+gpg_password="your_gpg_password_here"
+
+function encrypt_file {
+  echo "$gpg_password" | gpg --batch --yes --passphrase-fd 0 --symmetric --cipher-algo AES256 $password_file
+  rm $password_file
+}
+
+function decrypt_file {
+  echo "$gpg_password" | gpg --batch --yes --passphrase-fd 0 --output $password_file --decrypt $encrypted_password_file
+}
 
 function add_password {
   echo "サービス名を入力してください："
@@ -8,14 +21,24 @@ function add_password {
   read user_name
   echo "パスワードを入力してください："
   read -s password
+  
+  if [ -f $encrypted_password_file ]; then
+    decrypt_file
+  fi
+  
   echo "${service_name}:${user_name}:${password}" >> $password_file
+  encrypt_file
+
   echo "パスワードの追加は成功しました。"
 }
 
 function get_password {
   echo "サービス名を入力してください："
   read service_name
+
+  decrypt_file
   result=$(grep "^${service_name}:" $password_file)
+
   if [ -z "$result" ]; then
     echo "そのサービスは登録されていません。"
   else
@@ -24,6 +47,8 @@ function get_password {
     echo "ユーザー名：${arr[1]}"
     echo "パスワード：${arr[2]}"
   fi
+
+  encrypt_file
 }
 
 echo "パスワードマネージャーへようこそ！"
