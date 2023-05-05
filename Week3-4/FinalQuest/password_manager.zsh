ca7c25b5bf6ee3eae5a16abd3c3d4d24022e7d1b
Shin <84771278+Shin-mfc@users.noreply.github.com>
2023-05-06 00:39:28 +0900

    ステップ２

--- Week3-4/FinalQuest/password_manager.zsh
+++ Week3-4/FinalQuest/password_manager.zsh
@@ -1,22 +1,50 @@
 # ファイル名の設定
 password_file="passwords.txt"
 
+function add_password {
+  echo "サービス名を入力してください："
+  read service_name
+  echo "ユーザー名を入力してください："
+  read user_name
+  echo "パスワードを入力してください："
+  read -s password
+  echo "${service_name}:${user_name}:${password}" >> $password_file
+  echo "パスワードの追加は成功しました。"
+}
+
+function get_password {
+  echo "サービス名を入力してください："
+  read service_name
+  result=$(grep "^${service_name}:" $password_file)
+  if [ -z "$result" ]; then
+    echo "そのサービスは登録されていません。"
+  else
+    IFS=":" read -ra arr <<< "$result"
+    echo "サービス名：${arr[0]}"
+    echo "ユーザー名：${arr[1]}"
+    echo "パスワード：${arr[2]}"
+  fi
+}
+
 echo "パスワードマネージャーへようこそ！"
 
-# サービス名の入力
-echo "サービス名を入力してください："
-read service_name
+while true; do
+  echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
+  read choice
 
-# ユーザー名の入力
-echo "ユーザー名を入力してください："
-read user_name
-
-# パスワードの入力
-echo "パスワードを入力してください："
-read -s password
-
-# 情報をファイルに保存
-echo "${service_name}:${user_name}:${password}" >> $password_file
-
-# 完了メッセージ
-echo "Thank you!"
+  case $choice in
+    "Add Password")
+      add_password
+      ;;
+    "Get Password")
+      get_password
+      ;;
+    "Exit")
+      echo "Thank you!"
+      break
+      ;;
+    *)
+      echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+      ;;
+  esac
+done
